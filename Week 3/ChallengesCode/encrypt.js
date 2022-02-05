b4f9823e9efe0dca09822e5f74b5f3e3d8f453fb
Julio Nuñez <julionr14@gmail.com>
2022-02-05 00:31:18 -0600

    Update Week 4

--- Week 3/ChallengesCode/encrypt.js
+++ Week 3/ChallengesCode/encrypt.js
@@ -1,14 +1,21 @@
 var encryptThis = function(text) {
-    // Implement me! :)
-    let strArr = text.split(' ');
-    let encrypt = [];
-    for(let word of strArr){
-      if((/[a-zA-Z]/).test(word)){
-        encrypt.push(word.charCodeAt(0) + word[(word.length - 1)] + word.subString(2,(word.length-1)) + word[1]);
-      }
-    }
-    return encrypt.join(' ');
+  let strArr = text.split(' ');
+  let encrypt = [];
+  if(text.length == 1){
+    return (text.charCodeAt(0)).toString();
   }
-  console.log('Hola');
+  for(let word of strArr){
+    if((/[a-zA-Z]/).test(word)){
+      if(word.length == 1){
+        encrypt.push(word.charCodeAt(0));
+      }else if(word.length == 2){
+              encrypt.push(word.charCodeAt(0) + word[1] );
+      }else{
+          encrypt.push(word.charCodeAt(0) + word[(word.length - 1)] + word.substring(2,(word.length-1)) + word[1]);
+      }      
+    }
+  }
+  return encrypt.join(' ');
+}
   console.log(encryptThis("A wise old owl lived in an oak"));
   //
