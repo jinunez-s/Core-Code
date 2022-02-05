5a038e3c32da7a6a5423a4bce63aa6b92dde668a
Julio Nuñez <julionr14@gmail.com>
2022-02-05 01:01:33 -0600

    Update Week 4

--- Week 3/ChallengesCode/formatBArtLisaMaggie.js
+++ Week 3/ChallengesCode/formatBArtLisaMaggie.js
@@ -4,3 +4,5 @@ function list(names){
     return names.slice(0, len-1).map(p => p.name).join(', ') + (len > 1 ? ' & ' : '') + names[len-1].name;
   }
   list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]);
+
+  
--- Week 4/regularExpresion.js
+++ Week 4/regularExpresion.js
@@ -1,3 +1,14 @@
 let str = "The salad cost $9.99";
+//Check the next regex expressions!
+str.match(/\$\d.\d\d/);
+str.match(/\$\d\.\d\d/);
+str.match(/\$\d\.\d\d/)[0];
+str.match(/\$\d{1,3}\.\d\d/)[0];
 
-console.log(str.match(/\$\d.\d\d/));
+let str2 = "Contact customer support on 0800 300 500";
+str2.match(/\d{3,4}/g);
+str2.match(/\d{3,4}\s{3}/g); //three spaces of number
+console.log();
+
+let str3 = "You can contact me on Twitter @codebubb or james@juniordevelopercental.com";
+str3.match(/\S+@\S+\.\S+/);//(\S) --> match any caracter that is not an space in a string //Extract the email address in a string
