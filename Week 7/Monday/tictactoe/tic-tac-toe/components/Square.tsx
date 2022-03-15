51e0d0bf170de5c448fe83fee2b33bdf7ae440f1
Julio Nuñez <julionr14@gmail.com>
2022-03-14 19:23:17 -0600

    TicTacToe

---
+++ Week 7/Monday/tictactoe/tic-tac-toe/components/Square.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+type Player = 'X' | 'O' | null;
+
+function Square({
+    value, onClick, winner
+}:{
+    winner: Player;
+    value: Player;
+    onClick: () => void
+}){
+    if(!value){
+        return <button className="square" onClick={onClick} disabled={Boolean(winner)}   /> ;
+    }
+    return <button
+        className={`square square_${value.toLocaleLowerCase()}`}
+        disabled
+    >{value}</button>
+}
+export default Square;
